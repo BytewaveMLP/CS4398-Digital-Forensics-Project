22d32337f9e80512ff2374f49c5d5769875cd335
Eliot Partridge <byte@code.horse>
2021-09-26 14:25:57 -0500

    Project part 2

--- main.c
+++ main.c
@@ -12,7 +12,18 @@ Group 8: Eliot Partridge, Kent Templin
 #include <string.h>
 #include <errno.h>
 
-#define DEFAULT_BLOCK_SIZE 4096
+#define compare_sb_field(sb1, sb2, field) compare_sb_field_fmt(sb1, sb2, field, "%u")
+
+#define compare_sb_field_fmt(sb1, sb2, field, fmt) \
+	if (sb1->field != sb2->field) { \
+		printf("DIFF - " fmt " != " fmt "\n", sb1->field, sb2->field); \
+		result = 0; \
+	} else { \
+		printf("PASS - " fmt "\n", sb2->field); \
+	}
+
+static uint32_t backupSuperblockLocations[] = { 1, 3, 5, 7, 9, 25, 27, 49, 81, 125, 243, 343, 625, 729, 2401, 3125, 15625, 16807, };
+static size_t backupSuperblockLocationsLen = sizeof(backupSuperblockLocations) / sizeof(backupSuperblockLocations[0]);
 
 /**
  * Prints useful details about a given superblock
@@ -26,7 +37,7 @@ void print_superblock(struct ext3_super_block *sb) {
 	printf("\tFree block count: %u\n", sb->s_free_blocks_count);
 	printf("\tFree inode count: %u\n", sb->s_free_inodes_count);
 	printf("\tFirst data block: %u\n", sb->s_first_data_block);
-	printf("\tBlock size: %llu\n", 1024ll << sb->s_log_block_size);
+	printf("\tlog(Block size): %u\n", sb->s_log_block_size);
 	// printf("\tFragment size: %llu\n", 1024ll << sb->s_log_frag_size);
 	printf("\tBlocks per group: %u\n", sb->s_blocks_per_group);
 	// printf("\tFragments per group: %u\n", sb->s_frags_per_group);
@@ -47,8 +58,71 @@ void print_superblock(struct ext3_super_block *sb) {
 	printf("\tDefault GID: %u\n", sb->s_def_resgid);
 }
 
+int compare_superblocks(struct ext3_super_block *sb1, struct ext3_super_block *sb2) {
+	int result = 1;
+
+	printf("\tInode count: ");
+	compare_sb_field(sb1, sb2, s_inodes_count);
+
+	printf("\tBlock count: ");
+	compare_sb_field(sb1, sb2, s_blocks_count);
+
+	printf("\tReserved block count: ");
+	compare_sb_field(sb1, sb2, s_r_blocks_count);
+
+	printf("\tFree block count: ");
+	compare_sb_field(sb1, sb2, s_free_blocks_count);
+
+	printf("\tFree inode count: ");
+	compare_sb_field(sb1, sb2, s_free_inodes_count);
+
+	printf("\tFirst data block: ");
+	compare_sb_field(sb1, sb2, s_first_data_block);
+
+	printf("\tlog(Block size): ");
+	compare_sb_field(sb1, sb2, s_log_block_size);
+
+	printf("\tBlocks per group: ");
+	compare_sb_field(sb1, sb2, s_blocks_per_group);
+
+	printf("\tInodes per group: ");
+	compare_sb_field(sb1, sb2, s_inodes_per_group);
+
+	printf("\tLast mount time: ");
+	compare_sb_field(sb1, sb2, s_mtime);
+
+	printf("\tLast write time: ");
+	compare_sb_field(sb1, sb2, s_wtime);
+
+	printf("\tMount count: ");
+	compare_sb_field(sb1, sb2, s_mnt_count);
+
+	printf("\tMax mount count: ");
+	compare_sb_field(sb1, sb2, s_max_mnt_count);
+
+	// printf("\tState: ");
+	// compare_sb_field(sb1, sb2, s_state);
+
+	printf("\tErrors: ");
+	compare_sb_field(sb1, sb2, s_errors);
+
+	printf("\tOS: ");
+	compare_sb_field(sb1, sb2, s_creator_os);
+
+	printf("\tRevision: ");
+	compare_sb_field(sb1, sb2, s_rev_level);
+
+	printf("\tDefault UID: ");
+	compare_sb_field(sb1, sb2, s_def_resuid);
+
+	printf("\tDefault GID: ");
+	compare_sb_field(sb1, sb2, s_def_resgid);
+
+	return result;
+}
+
 int main(int argc, char *argv[]) {
-	// PART 1 (Eliot Partridge) - Read EXT3 superblock and print important details
+	// PART 1 - Read EXT3 superblock and print important details
 	struct ext3_super_block superblock;
 
 	if (argc != 2) {
@@ -77,12 +151,34 @@ int main(int argc, char *argv[]) {
 	printf("Superblock information:\n");
 	print_superblock(&superblock);
 
-	// PART 2 (Kent Templin) - Read backup superblocks and compare with primary
-
+	// PART 2 - Read backup superblocks and compare with primary superblock
 	uint64_t blockSize = 1024ll << superblock.s_log_block_size;
 	uint64_t blockCount = superblock.s_blocks_count;
 	uint64_t blockGroupSize = superblock.s_blocks_per_group;
 	uint64_t blockGroupCount = blockCount / superblock.s_blocks_per_group;
+
+	for (size_t i = 0; i < backupSuperblockLocationsLen; i++) {
+		printf("\n");
+
+		uint64_t location = backupSuperblockLocations[i];
+		if (location >= blockGroupCount) {
+			break;
+		}
+
+		struct ext3_super_block backupSuperblock;
+		retval = read_ext3_super_block(device, location * blockGroupSize * blockSize, &backupSuperblock);
+
+		if (retval == -1) {
+			printf("Could not read backup superblock %lu: %s\n", location, strerror(errno));
+			return EXIT_FAILURE;
+		} else if (retval == -2) {
+			printf("Could not read backup superblock %lu: Invalid magic number\n", location);
+			continue;
+		}
+
+		printf("Comparing superblocks at blockgroup 0 and blockgroup %lu\n", location);
+		compare_superblocks(&superblock, &backupSuperblock);
+	}
 
 	return EXIT_SUCCESS;
 }
